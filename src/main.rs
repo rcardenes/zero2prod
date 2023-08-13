90d05b79799f4fbc30a59f8b5508907a2d58c64b
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-08-13 08:15:45 -0400

    Make Clippy happy

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 //! main.rs
 
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
 use sqlx::postgres::PgPoolOptions;
 use std::{io, net::TcpListener};
 use zero2prod::configuration::get_configuration;
@@ -16,7 +15,7 @@ async fn main() -> Result<(), io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .connect_lazy(configuration.database.connection_string().expose_secret())
         .expect("Failed to create Postgres connection pool.");
     let address = format!(
         "{}:{}",
