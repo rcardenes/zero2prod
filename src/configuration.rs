d6c911a0ceb68901a1733d6073e1e9d81b1773f1
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-08-13 12:26:07 -0400

    Tone down SQLX's tracing

--- src/configuration.rs
+++ src/configuration.rs
@@ -2,7 +2,7 @@
 
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::postgres::{PgConnectOptions, PgSslMode};
+use sqlx::{postgres::{PgConnectOptions, PgSslMode}, ConnectOptions};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -44,7 +44,9 @@ impl DatabaseSettings {
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
-        self.without_db().database(&self.database_name)
+        let mut options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing_log::log::LevelFilter::Trace);
+        options
     }
 }
 
