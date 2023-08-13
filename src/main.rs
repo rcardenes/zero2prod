1834a6a9610104fdbd348f30ebb22fac6f09fa6b
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-08-13 07:08:21 -0400

    Reduce the timeout when connecting to the database

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 use secrecy::ExposeSecret;
 use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::{io, net::TcpListener};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -13,9 +14,10 @@ async fn main() -> Result<(), io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to create Postgres connection pool.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to create Postgres connection pool.");
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
