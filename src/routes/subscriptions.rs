cc736a135b1f0bc82e8875c5ba6f2fd4185ae96c
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-08-12 23:35:06 -0400

    Properly instrument the relevant functions in order to reduce the amount of boilerplate regarding tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,26 +10,25 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
         subscriber_email = %form.email,
         subscriber_name = %form.name,
-    );
+    )
+)]
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
 
-    let _request_span_guard = request_span.enter();
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
-    );
-
-    let query_span = tracing::info_span!("Saving new subscriber in the database");
-
-    let query = sqlx::query!(
+#[tracing::instrument(name = "Saving new subscriber in the database", skip(form, pool))]
+async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES($1, $2, $3, $4)
@@ -39,23 +38,11 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         form.name,
         Utc::now(),
     )
-    // We use `get_ref` to get an immutable reference to the `PgPool`
-    // wrapped by `web::Data`
-    .execute(pool.get_ref())
-    .instrument(query_span)
-    .await;
-
-    match query {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id,
-            );
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            tracing::error!("request_id {} - Failed to execute query: {e:?}", request_id,);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {e:?}");
+        e
+    })?;
+    Ok(())
 }
