7f942461cc90bda702bff88737cdc78e0b0b1783
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-08-14 13:08:12 -0400

    Validate emails

--- src/domain/new_subscriber.rs
+++ src/domain/new_subscriber.rs
@@ -1,8 +1,9 @@
 //! src/domain/new_subscriber.rs
 
-use crate::domain::subscriber_name::SubscriberName;
+use crate::domain::SubscriberEmail;
+use crate::domain::SubscriberName;
 
 pub struct NewSubscriber {
-    pub email: String,
+    pub email: SubscriberEmail,
     pub name: SubscriberName,
 }
